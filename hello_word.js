d897c1c853e78048bab5cecc86b7fb3b3a52d383
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-06-15 19:23:27 +0000

    first commit

---
+++ hello_word.js
@@ -0,0 +1,11 @@
+//pour l'afficher sur la console
+console.log('Hello world')
+
+//pour l'afficher sur la page HTML
+document.write('Hello world')
+
+//Pour l'afficher sur une boite de dialogue
+alert('Hello World')
+
+//pour l'afficher sur une boite de dialogue et invité l'utilisateur a saisir un message
+prompt('hello world')
